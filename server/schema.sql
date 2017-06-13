425798b8883b8e2844ff7c8e734d46aebaca359f
Hack Reactor Student <student@hackreactor.com>
2017-06-12 17:20:20 -0700

    Add multi-table schema and tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,32 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id int auto_increment NOT NULL,
+  name text,
+  PRIMARY KEY(id)
 );
+
+CREATE TABLE messages (
+   id int auto_increment NOT NULL,
+   user_id int NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE rooms (
+  id int auto_increment NOT NULL,
+  name text,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users_rooms (
+  user_id int NOT NULL,
+  room_id int NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
